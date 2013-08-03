67b6777a5b8e2c3a58dc0c5850bd0f3b102297f6
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2013-08-03 23:22:57 +0200

    add view/control

--- test-developpeur-JavaScript.js
+++ test-developpeur-JavaScript.js
@@ -4,45 +4,120 @@
 // À la fin des 10 secondes, je veux afficher le total de clignotements
 // Mais le code ne fonctionne pas complètement, je ne sais pas pourquoi :(
 
+// aBitOverIngineered
 (function(document, undefined) {
     'use strict';
 
     var app = window.app = window.app || {};
 
-    var totalTime = 10 * 1000,
-        delay = 2 * 1000,
-        classes = ['white', 'black'];
+    app.events = (function() {
+        var cache = {};
 
-    app.pulse = function(elms) {
-        var elmsLength = elms.length;
-        var counter = 0;
+        return {
+            emit: function(channel, args) {
+                if (!cache[channel]) { return; }
 
-        (function pulse(counter) {
-            // extends Array.prototype
-            for (var i = 0; i < elmsLength; i += 1) {
-                elms[i].className = classes[counter % 2];
-                elms[i].innerHTML = counter;
+                cache[channel].forEach(function(func) {
+                    func.apply(undefined, args);
+                });
+            },
+
+            listen: function(channel, func) {
+                if (!cache[channel]) {
+                    cache[channel] = [];
+                }
+
+                cache[channel].push(func);
             }
+        }
+    }());
 
-            if (!totalTime) {
-                alert(counter);
-            } else {
+    // pulsar
+    (function() {
+
+        var delay = 2 * 1000;
+
+        var pulse = function(elms) {
+            var counter = 0,
+                totalTime = 10 * 1000;
+
+            (function pulse(counter) {
+                if (!totalTime) {
+                    app.events.emit('pulse:end', [counter]);
+                    return;
+                }
+
+                counter += 1;
                 totalTime -= delay;
-                setTimeout(pulse, delay, counter += 1);
+
+                app.events.emit('pulse', [counter]);
+                setTimeout(pulse, delay, counter);
+            }(counter));
+        };
+
+        app.events.listen('pulse:start', pulse);
+
+    }());
+
+    // scene
+    (function() {
+        var rows = document.getElementsByTagName('li'),
+            rowsLength = rows.length,
+            classes = ['white', 'black'],
+            waitingMessage = rows[0].innerHTML;
+
+        var updateList = function(classname, text) {
+            for (var i = 0; i < rowsLength; i += 1) {
+                rows[i].className = classname;
+                if (text) {
+                    rows[i].innerHTML = text;
+                }
             }
-        }(counter));
-    }
-    // */
+        }
+
+        var onPulse = function(counter) {
+            updateList(classes[counter % 2], counter);
+        }
+
+        var onEnd = function(counter) {
+            alert(counter);
+            updateList(classes[0], waitingMessage);
+        }
+
+        app.events.listen('pulse', onPulse);
+        app.events.listen('pulse:end', onEnd);
+
+    }());
+
+    // control
+    (function() {
+
+        var btn = document.getElementById('launch');
+
+        var launch = function(e) {
+            e.preventDefault();
+            app.events.emit('pulse:start');
+        }
+
+        var started = function() {
+            btn.innerHTML = 'blinking...';
+            btn.removeEventListener('click', launch);
+        }
+
+        var ended = function() {
+            btn.innerHTML = 'start';
+            btn.addEventListener('click', launch);
+        }
+
+        btn.addEventListener('click', launch, false);
+
+        app.events.listen('pulse:start', started);
+        app.events.listen('pulse:end', ended);
+
+    }());
 
 }(document));
 
-(function(document, undefined) {
-    'use strict';
-    // configure totalTime, delay, classes...
-    var elms = document.getElementsByTagName('li');
-    app.pulse(elms);
-
-}(document));
 /*
 @TODO :
     - controller : button
