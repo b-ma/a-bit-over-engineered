f0f4b1cb461a074d487a76e6ee91cf40662ea966
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2013-08-03 11:23:49 +0200

    clean

--- test-developpeur-JavaScript.js
+++ test-developpeur-JavaScript.js
@@ -4,23 +4,6 @@
 // À la fin des 10 secondes, je veux afficher le total de clignotements
 // Mais le code ne fonctionne pas complètement, je ne sais pas pourquoi :(
 
-/*
-var total = currentId = clignotements = 0;
-var currentNode;
-var rows = document.getElementsByTagName('li');
-
-while(currentNode = rows.shift()) {
-  currentId = currentId + 1;
-  clignotements = currentId * 2 * 10;
-  total = currentId;
-
-  setInterval("currentNode.className = 'black';currentNode.innerHTML = clignotements", 200);
-  setInterval("currentNode.className = 'white';currentNode.innerHTML = clignotements", 400);
-}
-
-setTimeout("alert('Le total des clignotements est:' + clignotements)", 1000);
-// */
-
 (function(document, undefined) {
     'use strict';
 
@@ -28,25 +11,8 @@ setTimeout("alert('Le total des clignotements est:' + clignotements)", 1000);
 
     var totalTime = 10 * 1000,
         delay = 2 * 1000,
-        // rows = document.getElementsByTagName('li'),
-        // rowsLength = rows.length,
         classes = ['white', 'black'];
-        /*
-    app.pulse = function pulse(counter) {
-        for (var i = 0; i < rowsLength; i++) {
-            rows[i].className = classes[counter % 2];
-            rows[i].innerHTML = counter;
-        }
 
-        if (!totalTime) {
-            alert(counter);
-        } else {
-            totalTime -= delay;
-            setTimeout(pulse, delay, counter += 1);
-        }
-    };
-    // */
-    //*
     app.pulse = function(elms) {
         var elmsLength = elms.length;
         var counter = 0;
@@ -72,9 +38,10 @@ setTimeout("alert('Le total des clignotements est:' + clignotements)", 1000);
 
 (function(document, undefined) {
     'use strict';
-
+    // configure totalTime, delay, classes...
     var elms = document.getElementsByTagName('li');
     app.pulse(elms);
+
 }(document));
 /*
 @TODO :
