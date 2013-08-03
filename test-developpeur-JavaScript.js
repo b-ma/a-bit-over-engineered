c6b21b4982232b96decc4f564427e0f0ffc61d5a
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2013-08-03 11:21:18 +0200

    refactor structure

--- test-developpeur-JavaScript.js
+++ test-developpeur-JavaScript.js
@@ -24,13 +24,15 @@ setTimeout("alert('Le total des clignotements est:' + clignotements)", 1000);
 (function(document, undefined) {
     'use strict';
 
+    var app = window.app = window.app || {};
+
     var totalTime = 10 * 1000,
         delay = 2 * 1000,
-        rows = document.getElementsByTagName('li'),
-        rowsLength = rows.length,
+        // rows = document.getElementsByTagName('li'),
+        // rowsLength = rows.length,
         classes = ['white', 'black'];
-
-    (function pulse(counter) {
+        /*
+    app.pulse = function pulse(counter) {
         for (var i = 0; i < rowsLength; i++) {
             rows[i].className = classes[counter % 2];
             rows[i].innerHTML = counter;
@@ -42,6 +44,70 @@ setTimeout("alert('Le total des clignotements est:' + clignotements)", 1000);
             totalTime -= delay;
             setTimeout(pulse, delay, counter += 1);
         }
-    }(0));
+    };
+    // */
+    //*
+    app.pulse = function(elms) {
+        var elmsLength = elms.length;
+        var counter = 0;
+
+        (function pulse(counter) {
+            // extends Array.prototype
+            for (var i = 0; i < elmsLength; i += 1) {
+                elms[i].className = classes[counter % 2];
+                elms[i].innerHTML = counter;
+            }
+
+            if (!totalTime) {
+                alert(counter);
+            } else {
+                totalTime -= delay;
+                setTimeout(pulse, delay, counter += 1);
+            }
+        }(counter));
+    }
+    // */
 
 }(document));
+
+(function(document, undefined) {
+    'use strict';
+
+    var elms = document.getElementsByTagName('li');
+    app.pulse(elms);
+}(document));
+/*
+@TODO :
+    - controller : button
+    - popup with button to close
+    - create a communication object (pubsub or something)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
